1de11848c3b345f52f37dc9b7fdc0844cd8b2ddf
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-06-07 02:17:55 +0600

    Reusable Component

--- lib/views/splash/splash_screen.dart
+++ lib/views/splash/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterbloc_cleanarchitecture/config/components/internet_exception_widget.dart';
+
+import 'package:flutterbloc_cleanarchitecture/config/components/round_button.dart';
 import 'package:flutterbloc_cleanarchitecture/config/routes/routes_name.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,12 +15,24 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, RoutesName.homeScreen);
-              },
-              child: const Text("Home"))),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InterNetExceptionWidget(onPress: () {})
+          // RoundButton(
+          //   title: 'title',
+          //   onPress: () {},
+          //   height: 200,
+          // ),
+          // // const LoadingWidget(),
+          // Center(
+          //     child: TextButton(
+          //         onPressed: () {
+          //           Navigator.pushNamed(context, RoutesName.homeScreen);
+          //         },
+          //         child: const Text("Home"))),
+        ],
+      ),
     );
   }
 }
